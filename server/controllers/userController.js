ddc4b4cfedb36fd1d8a6601d8dd3329db187d63f
Victoria <rybalko.victoria@gmail.com>
2020-12-12 19:16:23 +0200

    Refactor user controller
    Fixed reference to user data
    Commented the lines with auth methods ( there is separate authController)
    Fixed async functions
    Fixed minor sequelize mistakes
    Autofixed formatting

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,9 @@
 const jwt = require('jsonwebtoken');
-const CONSTANTS = require('./../constants');
+const bcrypt = require('bcrypt');
+const moment = require('moment');
+const { Op } = require('sequelize');
+const uuid = require('uuid/v1');
+const CONSTANTS = require('../constants');
 const {
   sequelize,
   Contest,
@@ -8,92 +12,88 @@ const {
   Offer,
   Rating,
 } = require('../models');
+
 const NotFound = require('../errors/UserNotFoundError');
 const ServerError = require('../errors/ServerError');
 const UtilFunctions = require('../utils/functions');
 const NotEnoughMoney = require('../errors/NotEnoughMoney');
-const bcrypt = require('bcrypt');
 const NotUniqueEmail = require('../errors/NotUniqueEmail');
-const moment = require('moment');
-const uuid = require('uuid/v1');
-const controller = require('./../socketInit');
+const controller = require('../socketInit');
 const userQueries = require('./queries/userQueries');
 const bankQueries = require('./queries/bankQueries');
 const ratingQueries = require('./queries/ratingQueries');
 const BadRequestError = require('../errors/BadRequestError');
 
-module.exports.login = async (req, res, next) => {
-  try {
-    const foundUser = await userQueries.findUser({ email: req.body.email });
-    await userQueries.passwordCompare(req.body.password, foundUser.password);
-    const accessToken = jwt.sign(
-      {
-        firstName: foundUser.firstName,
-        userId: foundUser.id,
-        role: foundUser.role,
-        lastName: foundUser.lastName,
-        avatar: foundUser.avatar,
-        displayName: foundUser.displayName,
-        balance: foundUser.balance,
-        email: foundUser.email,
-        rating: foundUser.rating,
-      },
-      CONSTANTS.JWT_SECRET,
-      { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
-    );
-    await userQueries.updateUser({ accessToken: accessToken }, foundUser.id);
-    res.send({ token: accessToken });
-  } catch (err) {
-    next(err);
-  }
-};
-module.exports.registration = async (req, res, next) => {
-  try {
-    const newUser = await userQueries.userCreation(
-      Object.assign(req.body, { password: req.hashPass })
-    );
-    const accessToken = jwt.sign(
-      {
-        firstName: newUser.firstName,
-        userId: newUser.id,
-        role: newUser.role,
-        lastName: newUser.lastName,
-        avatar: newUser.avatar,
-        displayName: newUser.displayName,
-        balance: newUser.balance,
-        email: newUser.email,
-        rating: newUser.rating,
-      },
-      CONSTANTS.JWT_SECRET,
-      { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
-    );
-    await userQueries.updateUser({ accessToken: accessToken }, newUser.id);
-    res.send({ token: accessToken });
-  } catch (err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      next(new NotUniqueEmail());
-    } else {
-      next(err);
-    }
-  }
-};
+// module.exports.login = async (req, res, next) => {
+//   try {
+//     const foundUser = await userQueries.findUser({ email: req.body.email });
+//     await userQueries.passwordCompare(req.body.password, foundUser.password);
+//     const accessToken = jwt.sign(
+//       {
+//         firstName: foundUser.firstName,
+//         userId: foundUser.id,
+//         role: foundUser.role,
+//         lastName: foundUser.lastName,
+//         avatar: foundUser.avatar,
+//         displayName: foundUser.displayName,
+//         balance: foundUser.balance,
+//         email: foundUser.email,
+//         rating: foundUser.rating,
+//       },
+//       CONSTANTS.JWT_SECRET,
+//       { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
+//     );
+//     await userQueries.updateUser({ accessToken: accessToken }, foundUser.id);
+//     res.send({ token: accessToken });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+// module.exports.registration = async (req, res, next) => {
+//   try {
+//     const newUser = await userQueries.userCreation(
+//       Object.assign(req.body, { password: req.hashPass })
+//     );
+//     const accessToken = jwt.sign(
+//       {
+//         firstName: newUser.firstName,
+//         userId: newUser.id,
+//         role: newUser.role,
+//         lastName: newUser.lastName,
+//         avatar: newUser.avatar,
+//         displayName: newUser.displayName,
+//         balance: newUser.balance,
+//         email: newUser.email,
+//         rating: newUser.rating,
+//       },
+//       CONSTANTS.JWT_SECRET,
+//       { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
+//     );
+//     await userQueries.updateUser({ accessToken: accessToken }, newUser.id);
+//     res.send({ token: accessToken });
+//   } catch (err) {
+//     if (err.name === 'SequelizeUniqueConstraintError') {
+//       next(new NotUniqueEmail());
+//     } else {
+//       next(err);
+//     }
+//   }
+// };
 
 function getQuery(offerId, userId, mark, isFirst, transaction) {
-  const getCreateQuery = () =>
-    ratingQueries.createRating(
-      {
-        offerId: offerId,
-        mark: mark,
-        userId: userId,
-      },
-      transaction
-    );
-  const getUpdateQuery = () =>
-    ratingQueries.updateRating(
-      { mark: mark },
-      { offerId: offerId, userId: userId },
-      transaction
-    );
+  const getCreateQuery = () => ratingQueries.createRating(
+    {
+      offerId,
+      mark,
+      userId,
+    },
+    transaction,
+  );
+  const getUpdateQuery = () => ratingQueries.updateRating(
+    { mark },
+    { offerId, userId },
+    transaction,
+  );
   return isFirst ? getCreateQuery : getUpdateQuery;
 }
 
@@ -101,8 +101,10 @@ module.exports.changeMark = async (req, res, next) => {
   let sum = 0;
   let avg = 0;
   let transaction;
-  const { isFirst, offerId, mark, creatorId } = req.body;
-  const userId = req.tokenData.userId;
+  const {
+    isFirst, offerId, mark, creatorId,
+  } = req.body;
+  const { userId } = req.tokenPayload;
   try {
     transaction = await sequelize.transaction({
       isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED,
@@ -129,7 +131,7 @@ module.exports.changeMark = async (req, res, next) => {
     controller.getNotificationController().emitChangeMark(creatorId);
     res.send({ userId: creatorId, rating: avg });
   } catch (err) {
-    transaction.rollback();
+    await transaction.rollback();
     next(err);
   }
 };
@@ -138,7 +140,9 @@ module.exports.payment = async (req, res, next) => {
   const transaction = await sequelize.transaction();
   try {
     const {
-      body: { cvc, expiry, price, number },
+      body: {
+        cvc, expiry, price, number,
+      },
     } = req;
     const squadhelpCreditCardNumber = process.env.SQUADHELP_CREDIT_CARD_NUMBER;
 
@@ -160,31 +164,30 @@ module.exports.payment = async (req, res, next) => {
 
     await clientCreditCard.update(
       {
-        balance: Sequelize.literal(`"Banks"."balance" - ${price}`),
+        balance: Sequelize.literal(`"CreditCards"."balance" - ${price}`),
       },
-      { transaction }
+      { transaction },
     );
 
     await squadhelpCreditCard.update(
       {
-        balance: Sequelize.literal(`"Banks"."balance" + ${price}`),
+        balance: Sequelize.literal(`"CreditCards"."balance" + ${price}`),
       },
-      { transaction }
+      { transaction },
     );
 
     const orderId = uuid();
     req.body.contests.forEach((contest, index) => {
-      const prize =
-        index === req.body.contests.length - 1
-          ? Math.ceil(req.body.price / req.body.contests.length)
-          : Math.floor(req.body.price / req.body.contests.length);
+      const prize = index === req.body.contests.length - 1
+        ? Math.ceil(req.body.price / req.body.contests.length)
+        : Math.floor(req.body.price / req.body.contests.length);
       contest = Object.assign(contest, {
         status: index === 0 ? 'active' : 'pending',
-        userId: req.tokenData.userId,
+        userId: req.tokenPayload.userId,
         priority: index + 1,
-        orderId: orderId,
+        orderId,
         createdAt: moment().format('YYYY-MM-DD HH:mm'),
-        prize: prize,
+        prize,
       });
     });
     await Contest.bulkCreate(req.body.contests, transaction);
@@ -203,7 +206,7 @@ module.exports.updateUser = async (req, res, next) => {
     }
     const updatedUser = await userQueries.updateUser(
       req.body,
-      req.tokenData.userId
+      req.tokenPayload.userId,
     );
     res.send({
       firstName: updatedUser.firstName,
@@ -225,43 +228,43 @@ module.exports.cashout = async (req, res, next) => {
   try {
     transaction = await sequelize.transaction();
     const updatedUser = await userQueries.updateUser(
-      { balance: sequelize.literal('balance - ' + req.body.sum) },
-      req.tokenData.userId,
-      transaction
+      { balance: sequelize.literal(`balance - ${req.body.sum}`) },
+      req.tokenPayload.userId,
+      transaction,
     );
     await bankQueries.updateBankBalance(
       {
         balance: sequelize.literal(`CASE 
                 WHEN "cardNumber"='${req.body.number.replace(
-                  / /g,
-                  ''
-                )}' AND "expiry"='${req.body.expiry}' AND "cvc"='${
-          req.body.cvc
-        }'
+    / /g,
+    '',
+  )}' AND "expiry"='${req.body.expiry}' AND "cvc"='${
+  req.body.cvc
+}'
                     THEN "balance"+${req.body.sum}
                 WHEN "cardNumber"='${
-                  CONSTANTS.SQUADHELP_BANK_NUMBER
-                }' AND "expiry"='${
-          CONSTANTS.SQUADHELP_BANK_EXPIRY
-        }' AND "cvc"='${CONSTANTS.SQUADHELP_BANK_CVC}'
+  CONSTANTS.SQUADHELP_BANK_NUMBER
+}' AND "expiry"='${
+  CONSTANTS.SQUADHELP_BANK_EXPIRY
+}' AND "cvc"='${CONSTANTS.SQUADHELP_BANK_CVC}'
                     THEN "balance"-${req.body.sum}
                  END
                 `),
       },
       {
         cardNumber: {
-          [sequelize.Op.in]: [
+          [Op.in]: [
             CONSTANTS.SQUADHELP_BANK_NUMBER,
             req.body.number.replace(/ /g, ''),
           ],
         },
       },
-      transaction
+      transaction,
     );
     transaction.commit();
     res.send({ balance: updatedUser.balance });
   } catch (err) {
-    transaction.rollback();
+    await transaction.rollback();
     next(err);
   }
 };
