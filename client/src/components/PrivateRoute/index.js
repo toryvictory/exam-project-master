49950c9c079479ade13d3f8a2b1f76970593c395
Victoria <rybalko.victoria@gmail.com>
2020-12-29 17:27:18 +0200

    Fix bugs preventing private routes to reload on page refresh
    Fixed bugs that forced private route to redirect on login page after page refresh

--- client/src/components/PrivateRoute/index.js
+++ client/src/components/PrivateRoute/index.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useSelector}  from 'react-redux';
 import { Route, Redirect, useHistory } from 'react-router-dom';
 import Spinner from '../Spinner/Spinner';
 import checkRole from './helpers/checkRole';
 import { authSelector } from '../../selectors';
+import { REFRESH_TOKEN_KEY } from "../../constants";
 
 function PrivateRoute({ roles, ...rest }) {
-  const history = useHistory();
-  const { isFetching, user } = useSelector(authSelector);
+    const history = useHistory();
+    const { isFetching, user } = useSelector(authSelector);
 
-  if (isFetching) {
-    return <Spinner />;
-  }
-
-  if (user) {
-    if (roles && !checkRole(user.role, roles)) {
-      alert('Forbidden route'); // need pretty popup
-      history.goBack();
-      return;
+    let willFetchUser;
+    if (localStorage.getItem(REFRESH_TOKEN_KEY)) {
+        willFetchUser = true;
     }
 
-    return <Route {...rest} />;
-  }
+    if (user) {
+        if (roles && !checkRole(user.role, roles)) {
+            alert('Forbidden route'); // need pretty popup
+            history.goBack();
+            return;
+        }
+        return <Route {...rest} />;
+    }
 
-  return <Redirect to="/login" />;
+    if (willFetchUser || isFetching) {
+        return <Spinner/>;
+    }
+
+    return <Redirect to="/login"/>;
 }
 
 PrivateRoute.propTypes = {
-  roles: PropTypes.oneOfType([
-    PropTypes.shape({
-      include: PropTypes.arrayOf(PropTypes.string),
-      exclude: PropTypes.arrayOf(PropTypes.string),
-    }),
-    PropTypes.arrayOf(PropTypes.string),
-  ]),
+    roles: PropTypes.oneOfType([
+        PropTypes.shape({
+            include: PropTypes.arrayOf(PropTypes.string),
+            exclude: PropTypes.arrayOf(PropTypes.string),
+        }),
+        PropTypes.arrayOf(PropTypes.string),
+    ]),
 };
 
 export default PrivateRoute;
