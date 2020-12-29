05a8077153da79418e025de909d052308e64896e
Victoria <rybalko.victoria@gmail.com>
2020-12-29 20:03:32 +0200

    Refactor Header component
    Moved LoginButtons to separate component from Header component
    Performed minor refactorings

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,106 +1,22 @@
-import React, { useCallback } from 'react';
-import styles from './Header.module.sass';
+import React from 'react';
+import { useSelector } from "react-redux";
+import { authSelector } from "../../selectors";
 import { Link } from 'react-router-dom';
-import CONSTANTS, { ROLES } from '../../constants';
 import Icon from "@mdi/react";
 import { mdiPhone } from '@mdi/js';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { logoutRequest } from '../../actions/auth/authActionCreators';
-import { authSelector } from '../../selectors';
+import LoginButtons from "./LoginButtons/LoginButtons";
 import DropDownNavigation from "./DropDownNavigation/DropDownNavigation";
+import styles from './Header.module.sass';
+import CONSTANTS from '../../constants';
 
 function Header() {
-  const { isFetching, user } = useSelector(authSelector);
 
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const logoutAction = useCallback(() => {
-    history.push('/');
-    dispatch(logoutRequest());
-  }, [
-    dispatch,
-    logoutRequest,
-    history,
-  ]);
-
-  const renderLoginButtons = () => {
-    if (user) {
-      return (
-        <>
-          <div className={styles.userInfo}>
-            <img
-              src={
-                user.avatar === 'anon.png'
-                  ? CONSTANTS.ANONYM_IMAGE_PATH
-                  : `${CONSTANTS.publicURL}${user.avatar}`
-              }
-              alt="user"
-            />
-            <span>{`Hi, ${user.displayName}`}</span>
-            <img
-              src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-              alt="menu"
-            />
-            <ul>
-              <li>
-                <Link to="/dashboard" style={{ textDecoration: 'none' }}>
-                  <span>View Dashboard</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/account" style={{ textDecoration: 'none' }}>
-                  <span>My Account</span>
-                </Link>
-              </li>
-              <li>
-                <a
-                  href="http://www.google.com"
-                  target="_blank"
-                  style={{ textDecoration: 'none' }}
-                >
-                  <span>Messages</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://www.google.com"
-                  target="_blank"
-                  style={{ textDecoration: 'none' }}
-                >
-                  <span>Affiliate Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <span onClick={logoutAction}>Logout</span>
-              </li>
-            </ul>
-          </div>
-          <img
-            src={`${CONSTANTS.STATIC_IMAGES_PATH}email.png`}
-            className={styles.emailIcon}
-            alt="email"
-          />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <Link to="/login" style={{ textDecoration: 'none' }}>
-            <span className={styles.btn}>LOGIN</span>
-          </Link>
-          <Link to="/signup" style={{ textDecoration: 'none' }}>
-            <span className={styles.btn}>SIGN UP</span>
-          </Link>
-        </>
-      );
-    }
-  };
+  const { isFetching } = useSelector(authSelector);
 
   if (isFetching) {
     return null;
   }
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.fixedHeader}>
@@ -110,13 +26,13 @@ function Header() {
         </span>
         <a href="http://www.google.com">Read Announcement</a>
       </div>
-      <div className={styles.loginSignnUpHeaders}>
+      <div className={styles.loginSignUpHeaders}>
         <div className={styles.numberContainer}>
           <Icon path={mdiPhone} className={styles.icon}/>
           <span>(877)&nbsp;355-3585</span>
         </div>
         <div className={styles.userButtonsContainer}>
-          {renderLoginButtons()}
+          <LoginButtons/>
         </div>
       </div>
       <div className={styles.navContainer}>
@@ -129,9 +45,9 @@ function Header() {
         </Link>
         <div className={styles.leftNav}>
           <DropDownNavigation/>
-            <Link className={styles.startContestBtn} to="/startContest">
+          <Link className={styles.startContestBtn} to="/startContest">
               START CONTEST
-            </Link>
+          </Link>
         </div>
       </div>
     </div>
---
+++ client/src/components/Header/LoginButtons/LoginButtons.jsx
@@ -0,0 +1,98 @@
+import { useDispatch, useSelector } from "react-redux";
+import { userSelector } from "../../../selectors";
+import { Link, useHistory } from "react-router-dom";
+import React, { useCallback } from "react";
+import { logoutRequest } from "../../../actions/auth/authActionCreators";
+import styles from "../Header.module.sass";
+import CONSTANTS from "../../../constants";
+
+
+const LoginButtons = () => {
+
+    const user = useSelector(userSelector);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const logout = useCallback(() => {
+        history.push('/');
+        dispatch(logoutRequest());
+    }, [
+        dispatch,
+        history,
+    ]);
+
+    if (user) {
+        return (
+            <>
+                <div className={styles.userInfo}>
+                    <img
+                        src={
+                            user.avatar === 'anon.png'
+                                ? CONSTANTS.ANONYM_IMAGE_PATH
+                                : `${CONSTANTS.publicURL}${user.avatar}`
+                        }
+                        alt="user"
+                    />
+                    <span>{`Hi, ${user.displayName}`}</span>
+                    <img
+                        src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
+                        alt="menu"
+                    />
+                    <ul>
+                        <li>
+                            <Link to="/dashboard" style={{textDecoration: 'none'}}>
+                                <span>View Dashboard</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/account" style={{textDecoration: 'none'}}>
+                                <span>My Account</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <a
+                                href="http://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                                style={{textDecoration: 'none'}}
+                            >
+                                <span>Messages</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="http://www.google.com"
+                                target="_blank"
+                                rel="noreferrer"
+                                style={{textDecoration: 'none'}}
+                            >
+                                <span>Affiliate Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <span onClick={logout}>Logout</span>
+                        </li>
+                    </ul>
+                </div>
+                <img
+                    src={`${CONSTANTS.STATIC_IMAGES_PATH}email.png`}
+                    className={styles.emailIcon}
+                    alt="email"
+                />
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Link to="/login" style={{textDecoration: 'none'}}>
+                    <span>LOGIN</span>
+                </Link>
+                <Link to="/signup" style={{textDecoration: 'none'}}>
+                    <span>SIGN UP</span>
+                </Link>
+            </>
+        );
+    }
+};
+
+export default LoginButtons;
