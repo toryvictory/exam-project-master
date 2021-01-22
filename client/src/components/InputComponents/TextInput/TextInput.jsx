dc02a4428046337c6b8f5856713c862ccb4d8e39
Victoria <rybalko.victoria@gmail.com>
2021-01-22 20:55:52 +0200

    Change TextInput.jsx to accept css classes names as props as alternative to default classes

--- client/src/components/InputComponents/TextInput/TextInput.jsx
+++ client/src/components/InputComponents/TextInput/TextInput.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useField } from 'formik';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import styles from './TextInput.module.sass';
 
-const TextInput = ({ label, ...props }) => {
+const TextInput = ({
+  label, containerClass, labelClass, inputContainerClass, ...props
+}) => {
   const [field, meta] = useField(props);
   const inputClasses = classNames(styles.input, { [styles.notValid]: meta.touched && meta.error });
   return (
-    <div className={styles.container}>
-      <label className={styles.label} htmlFor={props.id || props.name}>{label}</label>
-      <div className={styles.inputContainer}>
+    <div className={containerClass || styles.container}>
+      <label className={labelClass || styles.label} htmlFor={props.id || props.name}>{label}</label>
+      <div className={inputContainerClass || styles.inputContainer}>
         <input className={inputClasses} {...field} {...props} />
         {meta.touched && meta.error ? (
           <div className={styles.error}>{meta.error}</div>
@@ -17,6 +20,20 @@ const TextInput = ({ label, ...props }) => {
       </div>
     </div>
   );
+};
+
+TextInput.propTypes = {
+  label: PropTypes.string,
+  containerClass: PropTypes.string,
+  labelClass: PropTypes.string,
+  inputContainerClass: PropTypes.string,
+};
+
+TextInput.defaultProps = {
+  label: null,
+  containerClass: '',
+  labelClass: '',
+  inputContainerClass: '',
 };
 
 export default TextInput;
