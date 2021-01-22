21786e81f1d6f83ffd6f0391c33fc0251bdc5ce6
Victoria <rybalko.victoria@gmail.com>
2021-01-22 20:54:36 +0200

    Change DateTimeInput.jsx to accept css classes names as props as alternative to default classes

--- client/src/components/InputComponents/DateTimeInput/DateTimeInput.jsx
+++ client/src/components/InputComponents/DateTimeInput/DateTimeInput.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { useField } from 'formik';
 import classNames from 'classnames';
 import Datetime from 'react-datetime';
+import PropTypes from 'prop-types';
 import styles from '../TextInput/TextInput.module.sass';
 
-const DateTimeInput = ({ label, ...props }) => {
+const DateTimeInput = ({
+  label, containerClass, labelClass, inputContainerClass, ...props
+}) => {
   const [field, meta] = useField(props);
   const inputClasses = classNames(styles.input, { [styles.notValid]: meta.touched && meta.error });
   return (
-    <div className={styles.container}>
-      <label className={styles.label} htmlFor={props.id || props.name}>{label}</label>
-      <div className={styles.inputContainer}>
+    <div className={containerClass || styles.container}>
+      <label className={labelClass || styles.label} htmlFor={props.id || props.name}>{label}</label>
+      <div className={inputContainerClass || styles.inputContainer}>
         <Datetime
           inputProps={{ className: inputClasses, readOnly: true }}
           {...field}
@@ -22,6 +25,20 @@ const DateTimeInput = ({ label, ...props }) => {
       </div>
     </div>
   );
+};
+
+DateTimeInput.propTypes = {
+  label: PropTypes.string,
+  containerClass: PropTypes.string,
+  labelClass: PropTypes.string,
+  inputContainerClass: PropTypes.string,
+};
+
+DateTimeInput.defaultProps = {
+  label: null,
+  containerClass: '',
+  labelClass: '',
+  inputContainerClass: '',
 };
 
 export default DateTimeInput;
