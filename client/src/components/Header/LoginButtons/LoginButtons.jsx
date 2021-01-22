22b7a2c6a007a4030dd1824d78625282eedc9e0c
Victoria <rybalko.victoria@gmail.com>
2021-01-22 14:40:15 +0200

    Add notification icon with number of events to relevant Header subcomponents: user avatar and Link to Events page

--- client/src/components/Header/LoginButtons/LoginButtons.jsx
+++ client/src/components/Header/LoginButtons/LoginButtons.jsx
@@ -1,16 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import React, { useCallback } from 'react';
+import React, { useCallback, useLayoutEffect } from 'react';
 import Icon from '@mdi/react';
 import { mdiChevronDown } from '@mdi/js';
 import { userSelector } from '../../../selectors';
 import { logoutRequest } from '../../../actions/auth/authActionCreators';
 import styles from '../Header.module.sass';
 import CONSTANTS from '../../../constants';
+import { getEvents } from '../../../actions/events/eventsActionCreators';
 
 const LoginButtons = () => {
   const user = useSelector(userSelector);
-  const { avatar, displayName } = user || {};
+  const { avatar, displayName, id } = user || {};
+  const events = useSelector((state) => state.eventsStore.events);
+  const eventsNotificationReducer = (acc, cur) => (cur.isNotificationOn ? acc + 1 : acc);
+  const eventsNotificationsCount = events.reduce(eventsNotificationReducer, 0);
+
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -22,17 +27,24 @@ const LoginButtons = () => {
     history,
   ]);
 
+  useLayoutEffect(() => { dispatch(getEvents(id)); }, []);
+
   if (user) {
     return (
       <>
         <div className={styles.userInfo}>
-          <img
-            src={
-                            avatar ? `${CONSTANTS.publicURL}${avatar}` : CONSTANTS.ANONYM_IMAGE_PATH
+          <div className={styles.avatarContainer}>
+            <img
+              src={
+                              avatar ? `${CONSTANTS.publicURL}${avatar}` : CONSTANTS.ANONYM_IMAGE_PATH
                         }
-            alt="user"
-          />
+              alt="user"
+            />
+            {!!eventsNotificationsCount
+            && <div className={`${styles.userNotification} ${styles.userNotification_avatar}`}>{eventsNotificationsCount}</div>}
+          </div>
           <span>{`Hi, ${displayName}`}</span>
+
           <Icon path={mdiChevronDown} className={styles.iconChevron} />
           <ul>
             <li>
@@ -48,6 +60,8 @@ const LoginButtons = () => {
             <li>
               <Link to="/events" style={{ textDecoration: 'none' }}>
                 <span>My Events</span>
+                {!!eventsNotificationsCount
+                && <span className={`${styles.userNotification} ${styles.userNotification_li}`}>{eventsNotificationsCount}</span>}
               </Link>
             </li>
             <li>
