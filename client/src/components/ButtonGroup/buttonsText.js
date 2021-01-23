7b6bd4984ce3a575ea54c7a6e5029ed9b443054e
Victoria <rybalko.victoria@gmail.com>
2021-01-23 17:27:46 +0200

    Add more items to buttons data to test scalability

--- client/src/components/ButtonGroup/buttonsText.js
+++ client/src/components/ButtonGroup/buttonsText.js
@@ -10,6 +10,18 @@ const data = [{
   answer: 'No',
   clarification: 'I am only looking for a name, not a Domain',
 },
+{
+  answer: 'YesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYes',
+  clarification: 'The Domain should exactly match the names The Domain should exactly match the names The Domain should exactly match the names The Domain should exactly match the names',
+},
+{
+  answer: 'YesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYes',
+  clarification: 'But minor variations are allowed (Recommended) But minor variations are allowed (Recommended) But minor variations are allowed (Recommended) But minor variations are allowed (Recommended)',
+},
+{
+  answer: 'NoNoNoNoNoNoNoNoNoNoNoNoNooNoNoNoNoNoNoNooNoNoNoNoNo',
+  clarification: 'I am only looking for a name, not a Domain I am only looking for a name, not a Domain I am only looking for a name, not a Domain',
+},
 ];
 
 export default data;
