82ed2834f8ac49af482855caa90216ac909e2084
Victoria <rybalko.victoria@gmail.com>
2021-01-23 13:52:50 +0200

    Add file with buttons text data

---
+++ client/src/components/ButtonGroup/buttonsText.js
@@ -0,0 +1,15 @@
+const data = [{
+  answer: 'Yes',
+  clarification: 'The Domain should exactly match the name',
+},
+{
+  answer: 'Yes',
+  clarification: 'But minor variations are allowed (Recommended)',
+},
+{
+  answer: 'No',
+  clarification: 'I am only looking for a name, not a Domain',
+},
+];
+
+export default data;
