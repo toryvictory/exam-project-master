06bcc4b548c559a4d37f32488948bde155c7afe2
Victoria <rybalko.victoria@gmail.com>
2021-01-24 16:31:18 +0200

    Create job function for daily logs movement to separate file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
+const schedule = require('node-schedule');
 const { app } = require('./app');
 const controller = require('./socketInit');
+const { loggerDailySchedule } = require('./errorLogger/loggerDailySchedule');
 
 require('./dbMongo/mongoose');
 
@@ -10,3 +12,5 @@ const port = process.env.PORT ?? 5000;
 
 server.listen(port);
 controller.createConnection(server);
+
+const job = schedule.scheduleJob('0 0 * * *', loggerDailySchedule);
