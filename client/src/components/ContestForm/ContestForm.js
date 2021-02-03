49eca92244ae208223e67ba93c093fccde83d9b3
Victoria <rybalko.victoria@gmail.com>
2021-02-03 16:20:35 +0200

    Fix scroll bug caused by auto browser scroll restoration and fix drop-down select bug in bundle contest creation

--- client/src/components/ContestForm/ContestForm.js
+++ client/src/components/ContestForm/ContestForm.js
@@ -1,18 +1,17 @@
-import React from "react";
-import CONSTANTS from "../../constants";
-import { connect } from "react-redux";
-import { getDataForContest } from "../../actions/actionCreator";
-import { withRouter } from "react-router-dom";
-import styles from "./ContestForm.module.sass";
-import Spinner from "../Spinner/Spinner";
-import { Field, reduxForm } from "redux-form";
-import FormInput from "../FormInput/FormInput";
-import SelectInput from "../SelectInput/SelectInput";
-import customValidator from "../../validators/validator";
-import Schems from "../../validators/validationSchems";
-import FieldFileInput from "../InputComponents/FieldFileInput/FieldFileInput";
-import FormTextArea from "../InputComponents/FormTextArea/FormTextArea";
-import TryAgain from "../TryAgain/TryAgain";
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import CONSTANTS from '../../constants';
+import styles from './ContestForm.module.sass';
+import Spinner from '../Spinner/Spinner';
+import FormInput from '../FormInput/FormInput';
+import SelectInput from '../SelectInput/SelectInput';
+import customValidator from '../../validators/validator';
+import Schems from '../../validators/validationSchems';
+import FieldFileInput from '../InputComponents/FieldFileInput/FieldFileInput';
+import FormTextArea from '../InputComponents/FormTextArea/FormTextArea';
+import TryAgain from '../TryAgain/TryAgain';
 
 let submitFunc;
 
@@ -22,29 +21,7 @@ class ContestForm extends React.Component {
     submitFunc = props.submitData;
   }
 
-  getPreference = () => {
-    const { contestType } = this.props;
-    switch (contestType) {
-      case CONSTANTS.NAME_CONTEST: {
-        this.props.getData({
-          characteristic1: "nameStyle",
-          characteristic2: "typeOfName",
-        });
-        break;
-      }
-      case CONSTANTS.TAGLINE_CONTEST: {
-        this.props.getData({ characteristic1: "typeOfTagline" });
-        break;
-      }
-      case CONSTANTS.LOGO_CONTEST: {
-        this.props.getData({ characteristic1: "brandStyle" });
-        break;
-      }
-    }
-  };
-
   componentDidMount() {
-    this.getPreference(this.props.contestType);
     this.props.initialize(this.props.defaultData);
   }
 
@@ -151,90 +128,89 @@ class ContestForm extends React.Component {
     const { isFetching, error } = this.props.dataForContest;
     const { handleSubmit, submitting } = this.props;
     if (error) return <TryAgain getData={this.getPreference} />;
-    else
-      return (
-        <>
-          {isFetching ? (
-            <Spinner />
-          ) : (
-            <div className={styles.formContainer}>
-              <form onSubmit={handleSubmit}>
-                <div className={styles.inputContainer}>
-                  <span className={styles.inputHeader}>Title of contest</span>
-                  <Field
-                    name="title"
-                    component={FormInput}
-                    type="text"
-                    label="Title"
-                    classes={{
-                      container: styles.componentInputContainer,
-                      input: styles.input,
-                      warning: styles.warning,
-                    }}
-                  />
-                </div>
-                <div className={styles.inputContainer}>
-                  <Field
-                    name="industry"
-                    component={SelectInput}
-                    classes={{
-                      inputContainer: styles.selectInputContainer,
-                      inputHeader: styles.selectHeader,
-                      selectInput: styles.select,
-                    }}
-                    header="Describe industry associated with your venture"
-                    optionsArray={this.props.dataForContest.data.industry}
-                  />
-                </div>
-                <div className={styles.inputContainer}>
-                  <span className={styles.inputHeader}>
-                    What does your company / business do?
-                  </span>
-                  <Field
-                    name="focusOfWork"
-                    component={FormTextArea}
-                    type="text"
-                    label="e.g. We`re an online lifestyle brand that provides stylish and high quality apparel to the expert eco-conscious shopper"
-                    classes={{
-                      container: styles.componentInputContainer,
-                      inputStyle: styles.textArea,
-                      warning: styles.warning,
-                    }}
-                  />
-                </div>
-                <div className={styles.inputContainer}>
-                  <span className={styles.inputHeader}>
-                    Tell us about your customers
-                  </span>
-                  <Field
-                    name="targetCustomer"
-                    component={FormTextArea}
-                    type="text"
-                    label="customers"
-                    classes={{
-                      container: styles.componentInputContainer,
-                      inputStyle: styles.textArea,
-                      warning: styles.warning,
-                    }}
-                  />
-                </div>
-                {this.renderSpecialInputs()}
+    return (
+      <>
+        {isFetching ? (
+          <Spinner />
+        ) : (
+          <div className={styles.formContainer}>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.inputContainer}>
+                <span className={styles.inputHeader}>Title of contest</span>
                 <Field
-                  name="file"
-                  component={FieldFileInput}
+                  name="title"
+                  component={FormInput}
+                  type="text"
+                  label="Title"
                   classes={{
-                    fileUploadContainer: styles.fileUploadContainer,
-                    labelClass: styles.label,
-                    fileNameClass: styles.fileName,
-                    fileInput: styles.fileInput,
+                    container: styles.componentInputContainer,
+                    input: styles.input,
+                    warning: styles.warning,
                   }}
-                  type="file"
                 />
-              </form>
-            </div>
-          )}
-        </>
-      );
+              </div>
+              <div className={styles.inputContainer}>
+                <Field
+                  name="industry"
+                  component={SelectInput}
+                  classes={{
+                    inputContainer: styles.selectInputContainer,
+                    inputHeader: styles.selectHeader,
+                    selectInput: styles.select,
+                  }}
+                  header="Describe industry associated with your venture"
+                  optionsArray={this.props.dataForContest.data.industry}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <span className={styles.inputHeader}>
+                  What does your company / business do?
+                </span>
+                <Field
+                  name="focusOfWork"
+                  component={FormTextArea}
+                  type="text"
+                  label="e.g. We`re an online lifestyle brand that provides stylish and high quality apparel to the expert eco-conscious shopper"
+                  classes={{
+                    container: styles.componentInputContainer,
+                    inputStyle: styles.textArea,
+                    warning: styles.warning,
+                  }}
+                />
+              </div>
+              <div className={styles.inputContainer}>
+                <span className={styles.inputHeader}>
+                  Tell us about your customers
+                </span>
+                <Field
+                  name="targetCustomer"
+                  component={FormTextArea}
+                  type="text"
+                  label="customers"
+                  classes={{
+                    container: styles.componentInputContainer,
+                    inputStyle: styles.textArea,
+                    warning: styles.warning,
+                  }}
+                />
+              </div>
+              {this.renderSpecialInputs()}
+              <Field
+                name="file"
+                component={FieldFileInput}
+                classes={{
+                  fileUploadContainer: styles.fileUploadContainer,
+                  labelClass: styles.label,
+                  fileNameClass: styles.fileName,
+                  fileInput: styles.fileInput,
+                }}
+                type="file"
+              />
+            </form>
+          </div>
+        )}
+      </>
+    );
   }
 }
 
@@ -242,28 +218,20 @@ const submit = (values) => {
   submitFunc(values);
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    contestStore: state.contestStore,
-    dataForContest: state.dataForContest,
-    initialValues: ownProps.defaultData,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getData: (data) => dispatch(getDataForContest(data)),
-  };
-};
+const mapStateToProps = (state, ownProps) => ({
+  contestStore: state.contestStore,
+  dataForContest: state.dataForContest,
+  initialValues: ownProps.defaultData,
+});
 
 export default withRouter(
   connect(
     mapStateToProps,
-    mapDispatchToProps
   )(
     reduxForm({
-      form: "contestForm",
+      form: 'contestForm',
       validate: customValidator(Schems.ContestSchem),
       onSubmit: submit,
-    })(ContestForm)
-  )
+    })(ContestForm),
+  ),
 );
--- client/src/pages/ContestCreation/ContestCreationPage.js
+++ client/src/pages/ContestCreation/ContestCreationPage.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import styles from './ContestCreationPage.module.sass';
 import {
   saveContestToStore,
   clearDataForContest,
+  getDataForContest,
 } from '../../actions/actionCreator';
 import NextButton from '../../components/NextButton/NextButton';
 import ContestForm from '../../components/ContestForm/ContestForm';
@@ -14,22 +15,71 @@ import BackButton from '../../components/BackButton/BackButton';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import Spinner from '../../components/Spinner/Spinner';
 
 const ContestCreationPage = (props) => {
   const history = useHistory();
+  const { pathname } = useLocation();
+  const {
+    dataForContest, contestType, getData, contestStore, bundleStore, resetForm,
+  } = props;
+  const { isFetching } = dataForContest;
+
+  useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+    return () => {
+      if ('scrollRestoration' in window.history) {
+        window.history.scrollRestoration = 'auto';
+      }
+    };
+  }, []);
+
+  const getPreference = () => {
+    switch (contestType) {
+      case CONSTANTS.NAME_CONTEST: {
+        getData({
+          characteristic1: 'nameStyle',
+          characteristic2: 'typeOfName',
+        });
+        break;
+      }
+      case CONSTANTS.TAGLINE_CONTEST: {
+        getData({ characteristic1: 'typeOfTagline' });
+        break;
+      }
+      case CONSTANTS.LOGO_CONTEST: {
+        getData({ characteristic1: 'brandStyle' });
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    getPreference(contestType);
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   const submitDataContest = (values) => {
-    props.saveContest({ type: props.contestType, info: values });
+    const { saveContest } = props;
+    saveContest({ type: contestType, info: values });
     history.push(
-      props.bundleStore.bundle[props.contestType] === 'payment'
+      bundleStore.bundle[contestType] === 'payment'
         ? '/payment'
-        : `${props.bundleStore.bundle[props.contestType]}Contest`,
+        : `${bundleStore.bundle[contestType]}Contest`,
     );
   };
 
-  !props.bundleStore.bundle && props.history.replace('/startContest');
-  const contestData = props.contestStore.contests[props.contestType]
-    ? props.contestStore.contests[props.contestType]
-    : { contestType: props.contestType };
+  !bundleStore.bundle && history.replace('/startContest');
+  const contestData = contestStore.contests[contestType]
+    ? contestStore.contests[contestType]
+    : { contestType };
+
+  if (isFetching) {
+    return <Spinner />;
+  }
+
   return (
     <div>
       <Header />
@@ -46,7 +96,8 @@ const ContestCreationPage = (props) => {
       <div className={styles.container}>
         <div className={styles.formContainer}>
           <ContestForm
-            contestType={props.contestType}
+            key={pathname}
+            contestType={contestType}
             submitData={submitDataContest}
             defaultData={contestData}
           />
@@ -66,13 +117,14 @@ const ContestCreationPage = (props) => {
 };
 
 const mapStateToProps = (state) => {
-  const { contestStore, bundleStore } = state;
-  return { contestStore, bundleStore };
+  const { contestStore, bundleStore, dataForContest } = state;
+  return { contestStore, bundleStore, dataForContest };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   saveContest: (data) => dispatch(saveContestToStore(data)),
   clearDataForContest: () => dispatch(clearDataForContest()),
+  getData: (data) => dispatch(getDataForContest(data)),
 });
 
 export default connect(
