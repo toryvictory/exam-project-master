cea851fe1a2e8fce2d045f17b63065c546cc9525
Victoria <rybalko.victoria@gmail.com>
2021-02-08 14:43:36 +0200

    Delete legacy auth-related code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,3 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const moment = require('moment');
 const { Op } = require('sequelize');
 const uuid = require('uuid/v1');
@@ -14,73 +12,11 @@ const {
   Rating,
   User,
 } = require('../models');
-
-const NotFound = require('../errors/UserNotFoundError');
 const ServerError = require('../errors/ServerError');
-const UtilFunctions = require('../utils/functions');
-const NotEnoughMoney = require('../errors/NotEnoughMoney');
-const NotUniqueEmail = require('../errors/NotUniqueEmail');
 const controller = require('../socketInit');
 const userQueries = require('./queries/userQueries');
 const bankQueries = require('./queries/bankQueries');
 const ratingQueries = require('./queries/ratingQueries');
-const BadRequestError = require('../errors/BadRequestError');
-
-// module.exports.login = async (req, res, next) => {
-//   try {
-//     const foundUser = await userQueries.findUser({ email: req.body.email });
-//     await userQueries.passwordCompare(req.body.password, foundUser.password);
-//     const accessToken = jwt.sign(
-//       {
-//         firstName: foundUser.firstName,
-//         userId: foundUser.id,
-//         role: foundUser.role,
-//         lastName: foundUser.lastName,
-//         avatar: foundUser.avatar,
-//         displayName: foundUser.displayName,
-//         balance: foundUser.balance,
-//         email: foundUser.email,
-//         rating: foundUser.rating,
-//       },
-//       CONSTANTS.JWT_SECRET,
-//       { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
-//     );
-//     await userQueries.updateUser({ accessToken: accessToken }, foundUser.id);
-//     res.send({ token: accessToken });
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// module.exports.registration = async (req, res, next) => {
-//   try {
-//     const newUser = await userQueries.userCreation(
-//       Object.assign(req.body, { password: req.hashPass })
-//     );
-//     const accessToken = jwt.sign(
-//       {
-//         firstName: newUser.firstName,
-//         userId: newUser.id,
-//         role: newUser.role,
-//         lastName: newUser.lastName,
-//         avatar: newUser.avatar,
-//         displayName: newUser.displayName,
-//         balance: newUser.balance,
-//         email: newUser.email,
-//         rating: newUser.rating,
-//       },
-//       CONSTANTS.JWT_SECRET,
-//       { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
-//     );
-//     await userQueries.updateUser({ accessToken: accessToken }, newUser.id);
-//     res.send({ token: accessToken });
-//   } catch (err) {
-//     if (err.name === 'SequelizeUniqueConstraintError') {
-//       next(new NotUniqueEmail());
-//     } else {
-//       next(err);
-//     }
-//   }
-// };
 
 module.exports.updateUser = async (req, res, next) => {
   try {
