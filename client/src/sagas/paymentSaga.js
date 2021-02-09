92e8ff7c7ac568178cca04c83e594b93238f829f
Victoria <rybalko.victoria@gmail.com>
2021-02-09 18:34:07 +0200

    Fixed bugs in updating user data in redux store after cashout

--- client/src/sagas/paymentSaga.js
+++ client/src/sagas/paymentSaga.js
@@ -1,5 +1,6 @@
 import { put } from 'redux-saga/effects';
 import ACTION from '../actions/actionTypes';
+import EDIT_USER_ACTION_TYPES from '../actions/user/editUserActionTypes';
 import history from '../browserHistory';
 import CONSTANTS from '../constants';
 import * as restController from '../api/rest/restController';
@@ -20,7 +21,7 @@ export function* cashoutSaga(action) {
   yield put({ type: ACTION.PAYMENT_ACTION_REQUEST, data: action.data });
   try {
     const { data } = yield restController.cashOut(action.data);
-    yield put({ type: ACTION.UPDATE_USER_DATA_SUCCESS, data });
+    yield put({ type: EDIT_USER_ACTION_TYPES.UPDATE_USER_DATA_SUCCESS, payload: { data } });
     yield put({ type: ACTION.CLEAR_PAYMENT_STORE });
     yield put({
       type: ACTION.CHANGE_PROFILE_MODE_VIEW,
