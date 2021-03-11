f62e1f83a9d96358b1cbd3548e250cf7c2aa492d
Victoria <rybalko.victoria@gmail.com>
2021-03-11 17:43:07 +0200

    Wrap OfferBox component into li element and adjust styles

--- client/src/pages/OfferModeration/OffersList/OffersList.jsx
+++ client/src/pages/OfferModeration/OffersList/OffersList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import OfferBox from '../../../components/OfferBox/OfferBox';
+import styles from './OffersList.module.sass';
 
 const OffersList = ({ offers }) => (
-  <ul>
-    {offers.map((offer) => <OfferBox key={offer.id} data={offer} />)}
+  <ul className={styles.list}>
+    {offers.map((offer) => <li key={offer.id}><OfferBox data={offer} /></li>)}
   </ul>
 );
 
